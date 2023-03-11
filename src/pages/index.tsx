ef4e7e0032b207198e5c27179a583c20c4876af0
zakiego <belummikir@gmail.com>
2023-03-11 14:37:49 +0800

    feat: add stringify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,17 +21,27 @@ const inter = Inter({ subsets: ["latin"] });
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const { data } = api.example.welcome.useQuery();
+  const data = api.example.welcome.useQuery();
 
   const [listName, setListName] = useState<string[]>([]);
 
   const [inputName, setInputName] = useState<string>("");
 
-  if (!data) {
+  if (!data.data) {
     return (
-      <Center>
-        <Box>Loading...</Box>
-      </Center>
+      <>
+        <p
+          style={{
+            color: "black",
+          }}
+        >
+          plan html loading
+        </p>
+        <p>{JSON.stringify(data)}</p>
+        <Center>
+          <Box>Loading...</Box>
+        </Center>
+      </>
     );
   }
 
@@ -41,65 +51,77 @@ export default function Home() {
   };
 
   return (
-    <Center pt="4">
-      <Stack>
-        <Heading>{data.greeting}</Heading>
-        <Stack
-          as="form"
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleOnSubmit();
-          }}
-        >
-          <FormControl>
-            <Input
-              maxW="xs"
-              placeholder="Masukkan nama"
-              onChange={(e) => {
-                setInputName(e.target.value);
-              }}
-              value={inputName}
-            />
-          </FormControl>
-          <Button type="submit" colorScheme="green">
-            Tambah
-          </Button>
-        </Stack>
+    <>
+      <p
+        style={{
+          color: "black",
+        }}
+      >
+        plan html success
+      </p>
+      <p>{JSON.stringify(data)}</p>
+      <Center pt="4">
+        <Stack>
+          <Heading>{data.data.greeting}</Heading>
+          <Stack
+            as="form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleOnSubmit();
+            }}
+          >
+            <FormControl>
+              <Input
+                maxW="xs"
+                placeholder="Masukkan nama"
+                onChange={(e) => {
+                  setInputName(e.target.value);
+                }}
+                value={inputName}
+              />
+            </FormControl>
+            <Button type="submit" colorScheme="green">
+              Tambah
+            </Button>
+          </Stack>
 
-        <Stack pt="4">
-          <Heading as="h2" fontSize="xl" color="blackAlpha.900">
-            Daftar Nama
-          </Heading>
-          {listName.map((name, index) => (
-            <HStack key={index} bg="gray.200" py="2" px="3" rounded="md">
-              <Text>{name}</Text>
-              <Spacer />
-              <Link href={`/profile`}>
-                <Tooltip label="Lihat Detail" aria-label="Lihat Detail">
+          <Stack pt="4">
+            <Heading as="h2" fontSize="xl" color="blackAlpha.900">
+              Daftar Nama
+            </Heading>
+            {listName.map((name, index) => (
+              <HStack key={index} bg="gray.200" py="2" px="3" rounded="md">
+                <Text>{name}</Text>
+                <Spacer />
+                <Link href={`/profile`}>
+                  <Tooltip label="Lihat Detail" aria-label="Lihat Detail">
+                    <IconButton
+                      aria-label="Details"
+                      icon={<ArrowForwardIcon />}
+                      variant="ghost"
+                      colorScheme="blue"
+                    />
+                  </Tooltip>
+                </Link>
+                <Tooltip label="Hapus" aria-label="Hapus">
                   <IconButton
-                    aria-label="Details"
-                    icon={<ArrowForwardIcon />}
+                    aria-label="Delete"
+                    icon={<DeleteIcon />}
                     variant="ghost"
-                    colorScheme="blue"
+                    colorScheme="red"
+                    onClick={() => {
+                      const newListName = listName.filter(
+                        (_, i) => i !== index,
+                      );
+                      setListName(newListName);
+                    }}
                   />
                 </Tooltip>
-              </Link>
-              <Tooltip label="Hapus" aria-label="Hapus">
-                <IconButton
-                  aria-label="Delete"
-                  icon={<DeleteIcon />}
-                  variant="ghost"
-                  colorScheme="red"
-                  onClick={() => {
-                    const newListName = listName.filter((_, i) => i !== index);
-                    setListName(newListName);
-                  }}
-                />
-              </Tooltip>
-            </HStack>
-          ))}
+              </HStack>
+            ))}
+          </Stack>
         </Stack>
-      </Stack>
-    </Center>
+      </Center>
+    </>
   );
 }
